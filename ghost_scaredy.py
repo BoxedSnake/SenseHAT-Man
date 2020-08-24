1779c956eb0b05e39f04fa14a722396cb177409d
BoxedSnake <williamte92@gmail.com>
2020-08-24 17:17:07 +0800

    ghost_scaredy complete

---
+++ ghost_scaredy.py
@@ -0,0 +1,52 @@
+"""
+    Ghost: Scaredy
+
+    This file defines the frames for Scaredy
+
+"""
+
+from sensible_colours import *
+
+blinky_frames = [
+    [
+        k, k, bd, bd, bd, rd, k, k,
+        k, bd, bd, bd, bd, bd, rd, k,
+        bd, bd, w, k, bd, w, k, rd,
+        bd, bd, w, w, bd, w, w, rd,
+        bd, bd, bd, bd, bd, bd, rd, rd,
+        bd, bd, bd, bd, bd, bd, rd, rd,
+        bd, bd, bd, bd, bd, bd, rd, rd,
+        bd, k, bd, bd, k, bd, rd, rd
+    ],
+    [
+        k, k, bd, bd, bd, rd, k, k,
+        k, bd, bd, bd, bd, r, rd, k,
+        bd, bd, w, k, bd, w, k, b,
+        bd, bd, w, w, bd, w, w, b,
+        bd, bd, bd, bd, bd, bd, b, b,
+        bd, bd, bd, bd, bd, bd, b, b,
+        bd, bd, bd, bd, bd, bd, b, b,
+        bd, k, bd, bd, bd, k, b, b
+    ],
+    [
+        k, k, bd, bd, bd, b, k, k,
+        k, bd, bd, bd, bd, bd, b, k,
+        bd, bd, k, w, bd, k, w, b,
+        bd, bd, w, w, bd, w, w, b,
+        bd, bd, bd, bd, bd, bd, b, b,
+        bd, bd, bd, bd, bd, bd, b, b,
+        bd, bd, bd, bd, bd, bd, b, b,
+        bd, bd, k, bd, bd, bd, k, b
+    ],
+    [
+        k, k, bd, bd, bd, b, k, k,
+        k, bd, bd, bd, bd, bd, b, k,
+        bd, bd, k, w, bd, k, w, b,
+        bd, bd, w, w, bd, w, w, b,
+        bd, bd, bd, bd, bd, bd, b, b,
+        bd, bd, bd, bd, bd, bd, b, b,
+        bd, bd, bd, bd, bd, bd, b, b,
+        bd, bd, k, bd, bd, k, b, b
+    ],
+
+]
