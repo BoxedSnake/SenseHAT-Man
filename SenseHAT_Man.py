98c20e32c8ff73bccac323dafca60febfa06ad5e
unknown <j162548@tafe.wa.edu.au>
2020-08-27 23:59:42 +0800

    SenseHAT_Man and SenseHAT_Man_Dying files added by @BoxedSnake

---
+++ SenseHAT_Man.py
@@ -0,0 +1,72 @@
+"""
+    SenseHAT Man
+
+    This file defines the frames for SenseHAT
+
+"""
+
+from sensible_colours import *
+
+blinky_frames = [
+    [
+        k, k, y, y, y, y, k, k,
+        k, y, y, y, y, y, y, k,
+        y, y, y, y, y, y, y, y,
+        y, y, y, y, y, y, y, y,
+        y, y, y, y, y, y, y, y,
+        y, y, y, y, y, y, y, y,
+        k, y, y, y, y, y, y, k,
+        k, k, y, y, y, y, k, k
+    ],
+    [
+        k, k, y, y, y, y, k, k,
+        k, y, y, y, y, y, y, k,
+        y, y, y, y, y, y, k, k,
+        y, y, y, y, y, k, k, k,
+        y, y, y, y, y, k, k, k,
+        y, y, y, y, y, y, k, k,
+        k, y, y, y, y, y, y, k,
+        k, k, y, y, y, y, k, k
+    ],
+    [
+        k, k, y, y, y, y, k, k,
+        k, y, y, y, y, y, k, k,
+        y, y, y, y, y, y, k, k,
+        y, y, y, y, y, k, k, k,
+        y, y, y, y, k, k, k, k,
+        y, y, y, y, y, k, k, k,
+        k, y, y, y, y, y, k, k,
+        k, k, y, y, y, y, k, k
+    ],
+    [
+        k, k, y, y, y, y, k, k,
+        k, y, y, y, y, y, y, k,
+        y, y, y, y, y, y, y, y,
+        y, y, y, y, y, y, y, y,
+        y, y, y, y, y, y, y, y,
+        y, y, y, y, y, y, y, y,
+        k, y, y, y, y, y, y, k,
+        k, k, y, y, y, y, k, k
+    ],
+	[
+        k, k, y, y, y, y, k, k,
+        k, y, y, y, y, y, k, k,
+        y, y, y, y, y, y, k, k,
+        y, y, y, y, y, k, k, k,
+        y, y, y, y, k, k, k, k,
+        y, y, y, y, y, k, k, k,
+        k, y, y, y, y, y, k, k,
+        k, k, y, y, y, y, k, k
+    ],
+    [
+        k, k, y, y, y, y, k, k,
+        k, y, y, y, y, y, y, k,
+        y, y, y, y, y, y, k, k,
+        y, y, y, y, y, k, k, k,
+        y, y, y, y, y, k, k, k,
+        y, y, y, y, y, y, k, k,
+        k, y, y, y, y, y, y, k,
+        k, k, y, y, y, y, k, k
+    ],
+]
+
---
+++ SenseHAT_Man_Dying.py
@@ -0,0 +1,92 @@
+"""
+    SenseHAT Man
+
+    This file defines the frames for SenseHAT
+
+"""
+
+from sensible_colours import *
+
+blinky_frames = [
+    [
+        k, k, y, y, y, y, k, k,
+        k, y, y, y, y, y, y, k,
+        y, y, y, y, y, y, y, y,
+        y, y, y, y, y, y, y, y,
+        y, y, y, y, y, y, y, y,
+        y, y, y, y, y, y, y, y,
+        k, y, y, y, y, y, y, k,
+        k, k, y, y, y, y, k, k
+    ],
+    [
+        k, k, y, y, y, y, k, k,
+        k, y, y, y, y, y, y, k,
+        y, y, y, y, y, y, k, k,
+        y, y, y, y, y, k, k, k,
+        y, y, y, y, y, k, k, k,
+        y, y, y, y, y, y, k, k,
+        k, y, y, y, y, y, y, k,
+        k, k, y, y, y, y, k, k
+    ],
+    [
+        k, k, y, y, y, y, k, k,
+        k, y, y, y, y, y, k, k,
+        y, y, y, y, y, y, k, k,
+        y, y, y, y, y, k, k, k,
+        y, y, y, y, k, k, k, k,
+        y, y, y, y, y, k, k, k,
+        k, y, y, y, y, y, k, k,
+        k, k, y, y, y, y, k, k
+    ],
+    [
+        k, k, y, y, k, k, k, k,
+        k, y, y, y, k, k, k, k,
+        y, y, y, y, k, k, k, k,
+        y, y, y, y, k, k, k, k,
+        y, y, y, y, k, k, k, k,
+        y, y, y, y, k, k, k, k,
+        k, y, y, y, k, k, k, k,
+        k, k, y, y, k, k, k, k
+    ],
+    [
+        k, k, k, k, k, k, k, k,
+        k, y, k, k, k, k, k, k,
+        y, y, y, k, k, k, k, k,
+        y, y, y, y, k, k, k, k,
+        y, y, y, y, k, k, k, k,
+        y, y, y, k, k, k, k, k,
+        k, y, k, k, k, k, k, k,
+        k, k, k, k, k, k, k, k
+    ],
+    [
+        k, k, k, k, k, k, k, k,
+        k, k, k, k, k, k, k, k,
+        k, k, k, k, k, k, k, k,
+        y, y, y, y, k, k, k, k,
+        y, y, y, y, k, k, k, k,
+        k, k, k, k, k, k, k, k,
+        k, k, k, k, k, k, k, k,
+        k, k, k, k, k, k, k, k
+    ],
+	[
+        k, k, k, k, k, k, k, k,
+        k, k, k, k, k, k, k, k,
+        k, k, k, k, k, k, k, k,
+        k, k, k, k, k, k, k, k,
+        k, k, k, k, k, k, k, k,
+        k, k, k, k, k, k, k, k,
+        k, k, k, k, k, k, k, k,
+        k, k, k, k, k, k, k, k
+    ],
+	[
+        k, y, k, y, k, y, k, k,
+        k, y, k, y, k, y, k, k,
+        k, y, k, y, k, y, k, k,
+        k, k, y, y, y, k, k, k,
+        k, k, k, y, k, k, k, k,
+        k, k, k, k, k, k, k, k,
+        k, k, k, k, k, k, k, k,
+        k, k, k, k, k, k, k, k
+    ],
+]
+
